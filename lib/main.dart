b3bb7eef831da2508f95e27ddb7bf3938aced9e6
Mr. Wiwathanakan Saithong <1166109060648@mail.rmutt.ac.th>
2025-10-16 11:37:59 +0700

    Add text color to 'Hello World!' in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,8 @@ class MainApp extends StatelessWidget {
     return const MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Text('Hello World!'),
+          child: Text('this is code from IStartingI', style: TextStyle(color: Colors.blue)),
+          
         ),
       ),
     );
